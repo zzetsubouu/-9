8c5b9ca91fc08d84282e09c1665c4434ddaad9c6
zzetsubouu <154281553+zzetsubouu@users.noreply.github.com>
2024-01-13 01:10:43 +0300

    Add files via upload

---
+++ 9/CompPract9/CompPract9/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompPract9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача #1
+            //Console.Write("Введите целое число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num > 0)
+            //{
+            //    int result = num + 1;
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    int result = num - 2;
+            //    Console.WriteLine(result);
+            //}
+
+            // Задача #2
+            //Console.Write("Введите первое число: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("Введите второе число: ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //if (num1 == num2)
+            //{
+            //    num1 = 0;
+            //    num2 = 0;
+            //    Console.WriteLine($"Первое число = {num1}\nВторое число = {num2}");
+            //}
+            //else
+            //{
+            //    int new1 = num1 + num2;
+            //    int new2 = num1 + num2;
+            //    Console.WriteLine($"Первое число = {new1}\nВторое число = {new2}");
+
+            //}
+
+            // Задача #3
+            //Console.Write("Введите целое число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num>0)
+            //{
+            //    num = num + 1;
+            //    Console.WriteLine(num);
+            //}
+            //if (num<0)
+            //{
+            //    num = num - 2;
+            //    Console.WriteLine(num);
+            //}
+            //if (num==0)
+            //{
+            //    num = 10;
+            //    Console.WriteLine(num);
+            //}    
+
+            // Задача #4
+            //Console.Write("Введите число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (num>=0)
+            //{
+            //    double result = 2 + 3 * Math.Sqrt(num);
+            //    Console.WriteLine(result);
+            //}    
+            //else
+            //{
+            //    double result = 1 - 2 * Math.Pow(num, 2);
+            //    Console.WriteLine(result);
+            //}
+
+            // Задача #5
+            //Console.Write("Введите число: ");
+            //int num = int.Parse(Console.ReadLine());
+            //if (0<=num && num<5)
+            //{
+            //    int result = num * 2;
+            //    Console.WriteLine(result);
+            //}
+            //else if(5<=num && num<10)
+            //{
+            //    Console.Write(num);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("0");
+            //}
+            Console.Read();
+        }
+    }
+}
---
+++ 9/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //задача 1
+            // a) ! (4 == 8)
+            //bool a = !(4 == 8);
+            //Console.WriteLine($"a) ! (4 == 8): {a}"); 
+
+            // b) (10 <= 10) | (4 >= 3)
+            //bool b = (10 <= 10) | (4 >= 3);
+            //Console.WriteLine($"b) (10 <= 10) | (4 >= 3): {b}"); 
+
+            // c) (5 < 8) | (4 > 3)
+            //bool c = (5 < 8) | (4 > 3);
+            //Console.WriteLine($"c) (5 < 8) | (4 > 3): {c}"); 
+
+            // d) (4 > 3) & (11 < 5)
+            //bool d = (4 > 3) & (11 < 5);
+            //Console.WriteLine($"d) (4 > 3) & (11 < 5): {d}"); 
+
+            //Console.Read();
+
+            //задача 2
+            //bool a = true;
+            //bool b = false;
+            //bool c = false;
+
+            //bool result = (a && c) || b;
+
+            //Console.WriteLine($"(a && c) || b: {result}");
+            //Console.Read();
+
+            //задача 3
+            //double a = 1.5;
+
+            //bool result = (a <= 1) && (a <= 2);
+
+            //Console.WriteLine($"(a <= 1) && (a <= 2): {result}");
+            //Console.Read();
+
+            //задача 4
+            //double a = 1.5;
+
+            //bool result = (a >= 1) && (a <= 0);
+
+            //Console.WriteLine($"(a >= 1) && (a <= 0): {result}");
+            //Console.Read();
+
+            //задача 5
+            //int a = 8;
+
+            //bool b = (a % 7 == 1) & (a / 7 == 1);
+
+            //Console.WriteLine(b);
+            //Console.Read();
+
+            //задача 6
+            //int a = 10;
+            //int b = 4;
+
+            //bool c = a == b;
+
+            //Console.WriteLine(c);
+            //Console.Read();
+
+            //задача 7
+            //int a = 10;
+            //int b = 4;
+
+            //bool c = a != b;
+            //bool d = a != 10;
+
+            //Console.WriteLine($"c: {c}"); 
+            //Console.WriteLine($"d: {d}");
+            //Console.Read();
+
+            //задача 8
+            //int a = 7;
+            //int b = 3;
+            //int c = 6;
+            //int x; 
+
+            //if (a < b && a > c)
+            //{
+            //    x = 4 * a - b * a;
+            //}
+            //else
+            //{
+            //    x = a - b;
+            //}
+
+            //Console.WriteLine($"Значение x: {x}"); 
+            //Console.Read();
+
+            //задача 9
+            //int amount = 15;
+
+
+            //int value = amount >= 10 ? 10 : 20;
+
+            //Console.WriteLine($"Значение: {value}");
+            //Console.Read();
+
+            //задача 10
+            //bool trueVal = true, falseVal = false;
+
+            //Console.WriteLine(trueVal & falseVal);
+            //Console.Read();
+        }
+    }
+}
+
+    
+
+
